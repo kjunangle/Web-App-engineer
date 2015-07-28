1cc91d7da70ca28bddc7fde8d99ab26d879799a3
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2015-07-28 22:04:59 +0700

    box office API V.1.0

---
+++ Box office API/test.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//$(document).ready(function() {
+//
+////$('#ul').load('ajax/http://www.nangdee.com/ #container');
+//
+//$.ajax({
+//    
+//   url:'http://nangdee.com',
+//    
+//
+//   success: function(data){
+//       
+//      data=$(data).filter('div#nd12Box_line2');
+//     $('#ul').append(data);
+//     alert('done');
+//     
+//   }
+//});
+//
+//
+//});
