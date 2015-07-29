6c584dfc98b51e57f09cce6c23205a7f2ea241ba
Palm <mommam456@gmail.com>
2015-07-29 21:25:19 +0700

    detail

--- MovieBlog/protected/views/movieBlog/MovieDetails.php
+++ MovieBlog/protected/views/movieBlog/MovieDetails.php
@@ -7,26 +7,21 @@ $this->breadcrumbs=array(
 
 ?>
 <h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+<div class="container">
+  <div id="tbody">
 
-<table>
-    
-    <thead>
-    <th>ID</th>
-    <th>#</th>
-      <th>Name</th>
-          <th>overview</th>
-          
-         <th> production_companies</th>
-          <th> popularity</th>
-           <th>Vote Average</th>
-    </thead>
-    
-    <tbody id="tbody">
-         
-        
-    </tbody>
-    </table>
-    <div id="digg">
+  </div>
+</div>
+<div class='container'>
+
+  <div class="list-group">
+  <a href="#" class="list-group-item disabled">
+    Digg
+  </a>
+  <div id="digg">
          
         
     </div>
+</div>
+    
+</div>
