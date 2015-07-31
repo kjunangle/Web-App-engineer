cb6006d41aed473627541c848722fa5e50927729
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2015-07-31 12:18:09 +0700

    delete for changes

--- MovieBlog/phpextension/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-mysql_connect("localhost:8889","root","root") or die("Cannot connect the Server");       
-mysql_select_db("movieblog_db") or die("Cannot select database");       
-mysql_query("set character set utf8");     
-?>   
