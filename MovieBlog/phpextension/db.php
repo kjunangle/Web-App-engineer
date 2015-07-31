6e2eb8b8c4fdab0c0ccce417230824a41018755a
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2015-07-31 12:54:17 +0700

    Revies1

--- MovieBlog/phpextension/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-mysql_connect("localhost:8889","movieblog_db","Q1w234") or die("Cannot connect the Server");       
-mysql_select_db("movieblog_db") or die("Cannot select database");       
-mysql_query("set character set utf8");     
-?>   
